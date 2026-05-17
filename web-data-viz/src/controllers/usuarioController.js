42447750479d2533489ba564afae5484df599c91
amandareys <amanda.reys@sptech.school>
2026-05-17 20:41:41 -0300

    adiciona bloqueio no quiz

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -20,11 +20,18 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
+                        let respondeuQuiz = false
+
+                        if (resultadoAutenticar[0].total_resultados >= 1) {
+                            respondeuQuiz = true;
+                        }
+
                         res.json({
                             id: resultadoAutenticar[0].id,
                             email: resultadoAutenticar[0].email,
                             nome: resultadoAutenticar[0].nome,
                             senha: resultadoAutenticar[0].senha,
+                            respondeuQuiz 
                         });
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -3,7 +3,17 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario as id, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT 
+            u.idUsuario AS id,
+            u.nome,
+            u.email,
+            COUNT(r.fk_usuario) AS total_resultados
+        FROM usuario u
+        LEFT JOIN resultado_usuario r 
+            ON r.fk_usuario = u.idUsuario
+        WHERE u.email = '${email}' 
+        AND u.senha = '${senha}'
+        GROUP BY u.idUsuario, u.nome, u.email;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
